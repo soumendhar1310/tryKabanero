c3c064fe552d42f65663e747dd7c45b9492c749d
soumendhar1310 <soumendhar1310@gmail.com>
2020-04-30 16:25:32 +0800

    try3

--- src/main/java/application/ExampleEndpoint.java
+++ src/main/java/application/ExampleEndpoint.java
@@ -8,6 +8,6 @@ public class ExampleEndpoint {
 
     @RequestMapping("/example")
     public String example() {
-        return "This is an example3";
+        return "This is an example4";
     }
 }
